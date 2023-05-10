2dcadbcf11e564becc62a53a8a5184cf9d591518
Emanuel Burgos <emaburgos17@hotmail.com>
2023-05-10 20:40:06 -0300

    Dashboard-user

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,24 +1,24 @@
 "use client"
 
 import Link from "next/link";
-import style from "./dashboard.module.css";
+
 
 export default function DashboardLayout({ children }) {
   return (
     <html lang="en">
-      <body className={style.body}>
-        <div className={style.contenedor}>
-          <div className={style.title}> 
+      <body >
+        <div >
+          <div > 
             <p>Dashboard</p>
           </div>
           <div>
-            <div className={style.boton}>
+            <div >
               <Link href="/dashboard/users">Users</Link>
             </div>
-            <div className={style.boton}>
+            <div >
               <Link href="/dashboard/payments">Payments</Link>
             </div>
-            <div className={style.boton}>
+            <div >
               <Link href="/dashboard/publications">Publications</Link>
             </div>
           </div>
