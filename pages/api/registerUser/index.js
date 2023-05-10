5ac347cdc8602c0fd5350b5c577e818e768feb3d
Franco Itria <francoitria01@gmail.com>
2023-05-10 16:42:05 -0300

    Ruta para el formulario de registro y crear usurio

---
+++ pages/api/registerUser/index.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+  const URL_BASE = "http://localhost:3000";
+  const { method } = req;
+  const { firstname, lastname, email, password, phoneNumber } = req.body;
+  if (method == "POST") {
+    try {
+      if (!firstname || !lastname || !email || !password || !phoneNumber) throw new Error("Faltan datos por completar");
+      const response = await fetch(`${URL_BASE}/api/user/${email}`);
+      const user = await response.json();
+      if (user) throw new Error("Cuenta ya registrada con ese email");
+      else {
+        const response = await fetch(`${URL_BASE}/api/user`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            firstname,
+            lastname,
+            email,
+            password,
+            phoneNumber,
+          }),
+        });
+        const newUser = await response.json();
+        res
+          .status(200)
+          .json({ message: "Usuario creado exitosamente", newUser });
+      }
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+}
