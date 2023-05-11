8bba4a1a62dd81c220334b91efcec8d2a9b29586
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-05-10 21:43:29 -0300

    renderizando con imagenes para probar

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,10 +1,17 @@
+import Cards from "@/components/Cards/Cards";
 import FilterBar from "@/components/Filter/FilterBar";
 
 function Home() {
-    return (
-        <div>
-            <FilterBar/>
-        </div>
-    );
+  return (
+    <div className="flex">
+      <div className="w-1/4">
+        <FilterBar/>            
+      </div>
+      <div className="w-3/4">
+        <Cards/>
+      </div>
+    </div>
+  );
 }
+
 export default Home;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,16 @@
 import Card from './Card.jsx';
 
 const tools = [
-  { title: 'Martillo', description: 'Herramienta de carpintería', imageUrl: 'https://example.com/martillo.jpg' },
-  { title: 'Sierra circular', description: 'Herramienta de carpintería', imageUrl: 'https://example.com/sierra-circular.jpg' },
-  { title: 'Taladro', description: 'Herramienta de electricidad', imageUrl: 'https://example.com/taladro.jpg' },
-  { title: 'Amoladora', description: 'Herramienta de electricidad', imageUrl: 'https://example.com/amoladora.jpg' },
+  { name: 'Martillo', category: 'Carpintería', rating: 4, price: { venta: 0, alquiler: 5 }, imageUrl: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQvMN1GaBANUUXkOb2yvTsA6eT6LVDQIW2lB8bp4Fn9AxDKOkd35STUjymVAuaYK6k5wB_xUDeaIcGjxqcIAd1COQWkqM_OGJk9OegpyotIZNjramnncl0IEbm2iocwCu-Zwg&usqp=CAc', description: 'Martillo de carpintería con mango de madera' },
+  { name: 'Sierra circular', category: 'Carpintería', rating: 5, price: { venta: 120, alquiler: 0 }, imageUrl: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSzLA__pyI7l9mqP3oDMx9o6MTBI3XpzaohFSRlVl3F5Cm6-I81gOBrGujE6LGTrMV6smj4CAQgJGtU1R1pV0kS97rfzCHwS61FaFM8-6H79ZOO7fwu0iuTXsgWUQ2C_IUpmwI&usqp=CAc', description: 'Sierra circular profesional con hoja de 12 pulgadas' },
+  { name: 'Taladro', category: 'Electricidad', rating: 4, price: { venta: 80, alquiler: 10 }, imageUrl: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRBarmuk7dbs9TBrq7b4mcACyo_BarqRJKcLad_x219aO-9GFgkmRG5aQZ4zzUVLhc7jlF2DfYgNfpukczq9FD3koPd8fPLPCgD6KHpdXjDLz75yUXWvfU0AiaI5F9OPsfphms&usqp=CAc', description: 'Taladro de percusión con cable de 10 pies' },
+  { name: 'Amoladora', category: 'Electricidad', rating: 3, price: { venta: 90, alquiler: 0 }, imageUrl: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR7XtSHjObVZ1gYSBGsiAjf1cYI66F5hAFeGP8kBtwabxOBdjYE8VypFH-OBj8Xf7M2mcL5w4RUEyEGrnQLkgM4lpE0wKApMy1Wgtmjs_czzJWBi9O66-W6tL6RUnfZPJ3rvw&usqp=CAc', description: 'Amoladora angular de 4.5 pulgadas con velocidad variable' },
+  { name: 'Pala', category: 'Excavación', rating: 2, price: { venta: 30, alquiler: 3 }, imageUrl: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQWsdFgFwKeh0QOobzWmz7Hg7-Eh49BHDpHQo3cumjg4Ygwea7UnGu3A9B0ZvwLxmS87SUwux3UGdIL_0qDlxr1nrwFEZmUvkye2ljFcEhyMrBmO0Oy3GF4R5KuUEfNl7PrXA&usqp=CAc', description: 'Pala cuadrada con mango de madera de 48 pulgadas' },
+  { name: 'Martillo perforador', category: 'Excavación', rating: 0, price: { venta: 150, alquiler: 20 }, imageUrl: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRmUlklTGBNEDLcsUn6yeRL9ZrDGeUwk4k4QHz_2tquPp5hSC1REqd8eFuez3FFUwT6bE_Dt4n794uAvPOJ6MMVWzqqSmwHlxN1QwJ1FEQHn9gtv-qimIIixVE3toO52HEWk-w&usqp=CAc', description: 'Martillo perforador de alta potencia con mandril de 1/2 pulgada' },
+  { name: 'Cortacésped', category: 'Jardinería', rating: 4, price: { venta: 0, alquiler: 30 }, imageUrl: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRVszEOu1Pe7afrlEKJcGK__yfbJ4n62G1HTxzlT6N4TTSDmOPLTlh_iOcl4R3aCoWhUCpeT_luIlZmlh8grNTOIIEKpKMeJYdQ9YYQt7CpX7wY69myeitr9zaSaqIYAS_bPIY&usqp=CAc', description: 'Cortacésped a gasolina de 21 pulgadas con tracción trasera' },
+  { name: 'Tijeras de podar', category: 'Jardinería', rating: 0, price: { venta: 0, alquiler: 5 }, imageUrl: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ_9NAmeJK2WFmkzY3OUrErWzySJyH3-G-GDKoofCkAPGa_IH5Rq7SprK5Hn3jebGp9UhLkNsMUzzA-n1OrkLP8A6ETCBCMFq0NEiTfkGlP79Zd6EqwkN81wot4XC2Rcv5urrs&usqp=CAc', description: 'Tijeras de podar de acero con mango ergonómico' }
 ];
+
 
 const Cards = () => {
   return (
