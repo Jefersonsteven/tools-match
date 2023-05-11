c34f47f5a245be0e02dd7a2f0206ff50b3574422
Gfrey <gfreycode@gmail.com>
2023-05-11 08:39:09 -0500

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
 "use client";
-import Footer from "@/components/footer/Footer";
 import LandingPage from "./landing/Landing";
-import Navbar from "@/components/navbar/Navbar";
-import Team from "../components/team/team";
 
 export default function Landing() {
   return (
     <>
-      <LandingPage />;     
+      <LandingPage />;
     </>
   );
 }
