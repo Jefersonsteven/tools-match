9f934183dcd5b910691c2b408363f4736976f658
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-05-11 02:19:11 -0300

    ya se parece mas a figma

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-const Card = ({ title, description, imageUrl }) => {
+const Card = ({ name, description, imageUrl, price, sale, rental }) => {
   return (
-    <div className={styles.card}>
-      <img src={imageUrl} alt={title} className={styles.cardImage} />
+    <div className={`${styles.cardContainer} bg-white rounded-md p-4`}>
+      <img src={imageUrl} alt={name} className={`${styles.cardImage} rounded-md`} />
       <div className={styles.cardContent}>
-        <h2 className={styles.cardTitle}>{title}</h2>
-        <p className={styles.cardDescription}>{description}</p>
+        <h2 className={styles.cardName}>{name}</h2>       
+      
+      <h2 className={styles.cardPrice}>
+        <span className={styles.cardPrice}> ${price} </span>
+        {rental > 0 ? (
+          <span className="text-sm text-gray-500">
+            Alquiler {rental} {rental === 1 ? 'día' : 'días'} {rental > 0 && sale > 0 && '|'}
+          </span>
+        ) : null}
+        {sale > 0 ? (
+          <span className="text-sm text-gray-500">
+            Venta {sale} {sale === 1 ? 'día' : 'días'}
+          </span>
+        ) : null}
+        {rental > 0 && <span className="text-sm text-gray-500">por día</span>}
+      </h2>
       </div>
     </div>
   );
