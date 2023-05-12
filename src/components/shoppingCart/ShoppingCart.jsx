4d4088eff9fafac5361150932c1154f64aeef722
Adri-ESM <anggivmorales@gmail.com>
2023-05-11 21:05:52 -0400

    creating shoppingCart branch

--- src/components/shoppingCart/ShoppingCart.jsx
+++ src/components/shoppingCart/ShoppingCart.jsx
@@ -1,6 +1,12 @@
 import style from "./ShoppingCart.module.css";
-
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
 export default function ShoppingCart() {
-  return;
-  <div>ShoppingCart</div>;
+  return (
+    <div>
+      <Header />
+      ShoppingCart
+      <Footer />
+    </div>
+  );
 }
