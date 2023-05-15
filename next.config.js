a727076435a16310619f708525114eef7d049f69
Gfrey <gfreycode@gmail.com>
2023-05-14 21:11:13 -0500

    done

--- next.config.js
+++ next.config.js
@@ -5,17 +5,7 @@ const nextConfig = {
   },
   images: {
     domains: [
-      "us.123rf.com",
       "res.cloudinary.com",
-      "ejemplo.com","thumbs.dreamstime.com",
-      "www.beta-tools.com","www.evans.com.co",
-      "images-na.ssl-images-amazon.com", 
-      "cdn3.iconfinder.com",
-      "encrypted-tbn0.gstatic.com",
-      "images.unsplash.com",
-      "encrypted-tbn3.gstatic.com",
-      "encrypted-tbn2.gstatic.com",
-      "encrypted-tbn1.gstatic.com",
     ],
   },
   env: {
