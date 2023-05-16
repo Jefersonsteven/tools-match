0d454e11d84cd0d748865885052a8754286612ce
Adri-ESM <anggivmorales@gmail.com>
2023-05-16 15:40:00 -0400

    creating pagination component

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -5,7 +5,7 @@ const AppContext = createContext();
 
 function AppProvider({ children }) {
   // * El id del usuario de la Sesion
-  const [userId, setUserId] = useState("932a3adf-9203-4b25-89ca-777b00411730");
+  const [userId, setUserId] = useState("33df4e67-e7c8-4ff6-9e64-476fb28389ce");
 
   // * Formulario para crear publicaciones */
   const [form, setForm] = useState({
@@ -41,66 +41,65 @@ function AppProvider({ children }) {
   const [name, setName] = useState("");
   // *----------------------------------* //
 
-
   //* Crear estado para saber si el usuario esta logeado o no (booleano)
   const [userSession, setUserSession] = useState(false);
 
-    const [title, setTitle] = useState('');
-    const [cards, setCards] = useState([]);
-    const [selectedType, setSelectedType] = useState('');    
-    const [searchTerm, setSearchTerm] = useState(''); 
-    const [filteredCards, setFilteredCards] = useState(cards)   
-    const [filter, setFilter] = useState('')
+  const [title, setTitle] = useState("");
+  const [cards, setCards] = useState([]);
+  const [selectedType, setSelectedType] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredCards, setFilteredCards] = useState(cards);
+  const [filter, setFilter] = useState("");
 
+  const tools = async () => {
+    const response = await axios.get("http://localhost:3000/api/admin/post");
+    return response.data;
+  };
 
-    const tools = async () => {
-      const response = await axios.get('http://localhost:3000/api/admin/post');
-      return response.data;
-    }
-
-    
-    return (
-        <AppContext.Provider value={{            
-            postDetail,
-            setPostDetail,
-            cards,
-            setCards,
-            selectedCategory,
-            setSelectedCategory,
-            title,
-            setTitle,
-            userId,
-            setUserId,
-            selectedType,
-            setSelectedType,
-            sortBy,
-            setSortBy,
-            searchTerm,
-            setSearchTerm,
-            tools,
-            filteredCards,
-            setFilteredCards,
-            rent,
-            setRent,
-            sale,
-            setSale,
-            name,
-            setName,
-            filter,
-            setFilter,    
-            userId,
-            setUserId,
-            userSession,
-            setUserSession,
-            userData,
-            setUserData,
+  return (
+    <AppContext.Provider
+      value={{
+        postDetail,
+        setPostDetail,
+        cards,
+        setCards,
+        selectedCategory,
+        setSelectedCategory,
+        title,
+        setTitle,
+        userId,
+        setUserId,
+        selectedType,
+        setSelectedType,
+        sortBy,
+        setSortBy,
+        searchTerm,
+        setSearchTerm,
+        tools,
+        filteredCards,
+        setFilteredCards,
+        rent,
+        setRent,
+        sale,
+        setSale,
+        name,
+        setName,
+        filter,
+        setFilter,
+        userId,
+        setUserId,
+        userSession,
+        setUserSession,
+        userData,
+        setUserData,
         form,
         setForm,
         errors,
-        setErrors
-        }}>
-            {children}
-        </AppContext.Provider >   
+        setErrors,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
   );
 }
 
