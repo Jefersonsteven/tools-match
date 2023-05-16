dbf3edb71ee3ca20ef2216731d916c6f30227124
Rai5559 <icexbee1@gmail.com>
2023-05-16 12:17:25 -0600

    static map image

--- pages/api/maps/mapUtil.js
+++ pages/api/maps/mapUtil.js
@@ -1,29 +1,16 @@
 import axios from "axios";
 
-const getLinkMapByZipCode = async (zipCode, country) => {
+const getStaticMapUrlByZipCode = async (zipCode, country) => {
   const apiKey = process.env.GOOGLE_API_KEY;
-  const encodedCountry = country;
-  const encodedZipCode = zipCode;
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?components=country:"${encodedCountry}"|postal_code:"${encodedZipCode}"&key=${apiKey}`;
+  const url = `https://maps.googleapis.com/maps/api/staticmap?center=${zipCode},${country}&zoom=12&size=600x400&maptype=roadmap&markers=color:red%7C&key=${apiKey}`;
 
   try {
     const response = await axios.get(url);
-    const data = response.data;
-
-    if (data.results.length > 0) {
-      const lat = data.results[0].geometry.location.lat;
-      const lng = data.results[0].geometry.location.lng;
-      const mapLink = `https://www.google.com/maps?q=${lat},${lng}`;
-      return mapLink;
-    } else {
-      throw new Error(
-        "No se encontraron resultados para el código postal y país especificados."
-      );
-    }
+    return response.config.url;
   } catch (error) {
-    console.error("Error al obtener el enlace del mapa:", error.message);
+    console.error("Error al obtener la URL del mapa estático:", error.message);
     throw error;
   }
 };
 
-export default getLinkMapByZipCode;
+export default getStaticMapUrlByZipCode;
