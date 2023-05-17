f91d7d47f9d164c392f1deffcca3b331584cc38a
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-05-17 14:08:11 -0300

    formReview ok

--- src/components/FormReview.jsx
+++ src/components/FormReview.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import { RiStarFill } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
 
 const FormReview = () => {
   const [rating, setRating] = useState(0);
   const [content, setContent] = useState("");
-  const [date, setDate] = useState("");
+  const [isOpen, setIsOpen] = useState(true);
+  const currentDate = new Date().toISOString().split("T")[0];
 
   const handleRatingClick = (value) => {
-    setRating(value);
+    if (rating === value) {
+      // Si se hace clic en la misma estrella, se colorea una menos
+      setRating(value - 1);
+    } else {
+      setRating(value);
+    }
   };
 
   const handleSubmit = (e) => {
@@ -17,42 +24,79 @@ const FormReview = () => {
     // Reiniciamos los campos después de enviar
     setRating(0);
     setContent("");
-    setDate("");
   };
 
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <h2>Leave a Review</h2>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="relative w-2/3 max-w-lg bg-green-80 p-8 rounded-lg shadow">
+        <div className="absolute top-0 right-0 mt-2 mr-2">
+          <button
+            className="bg-yellow-500 rounded-full p-2"
+            onClick={handleClose}
+          >
+            <AiOutlineClose className="text-black" />
+          </button>
+        </div>
+        <h2 className="text-2xl font-bold mb-4">Opinar y Calificar</h2>
         <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="date">Date:</label>
-            <input
-              type="text"
-              id="date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-            />
+          <div className="mb-4">
+            <label htmlFor="date" className="block mb-1 text-sm font-medium">
+              Fecha:
+            </label>
+            <div className="w-full border bg-white border-gray-300 rounded p-2 text-sm">
+              <input
+                type="text"
+                id="date"
+                value={currentDate}
+                readOnly
+                className="w-full bg-white cursor-text"
+              />
+            </div>
           </div>
-          <div>
-            <label htmlFor="content">Content:</label>
+          <div className="mb-4">
+            <label htmlFor="content" className="block mb-1 text-sm font-medium">
+              Reseña:
+            </label>
             <textarea
               id="content"
               value={content}
               onChange={(e) => setContent(e.target.value)}
+              className="w-full border border-gray-300 rounded p-2 text-sm h-40"
+              maxLength={500}
             ></textarea>
           </div>
-          <div>
-            <label>Rating:</label>
+          <div className="mb-4 flex justify-center">
             {[1, 2, 3, 4, 5].map((value) => (
               <RiStarFill
                 key={value}
-                className={`star-icon ${value <= rating ? "selected" : ""}`}
+                className={`star-icon text-4xl ${
+                  value <= rating ? "text-yellow-500" : "text-gray-400"
+                }`}
                 onClick={() => handleRatingClick(value)}
               />
             ))}
           </div>
-          <button type="submit">Submit</button>
+          <div className="flex justify-center
+
+
+
+">
+            <button
+              type="submit"              
+              className="bg-black hover:bg-gray-800 text-white font-bold py-1 px-3 rounded-full text-xs"
+              style={{ maxWidth: "200px" }}
+            >
+              Publicar Reseña
+            </button>
+          </div>
         </form>
       </div>
     </div>
