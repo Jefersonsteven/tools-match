fbc4446d6a6cf44c6bae412a312f9d22c0b43091
Gfrey <gfreycode@gmail.com>
2023-05-17 12:11:36 -0500

    deploy

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -5,27 +5,16 @@ import saveInLocalStorage from "./assets/saveInLocalStorage";
 import removeFromLocalStorage from "./assets/removeFromLocalStorage";
 import endSession from "./assets/endSession";
 
+import axios from "axios";
 const AppContext = createContext();
 
 function AppProvider({ children }) {
   // * Detalles de la publicación
   const [postDetail, setPostDetail] = useState({});
-  const [userData, setUserData] = useState(null);
-  const [userId, setUserId] = useState(null);
-  const [selected, setSelected] = useState()
-
-  if (typeof window !== 'undefined') {
-    // Acceso a localStorage aquí
-    const storedToken = localStorage.getItem("token");
-    const storedId = localStorage.getItem("id");
-
-    // Resto del código que depende de localStorage
-    if (storedId && storedToken) {
-      setUserData(storedToken)
-      setUserId(storedId)
-    }
-  }
-
+  const [userData, setUserData] = useState(
+    JSON.parse(localStorage.getItem("token"))
+  );
+  const [userId, setUserId] = useState(JSON.parse(localStorage.getItem("id")));
 
   // * Formulario para crear publicaciones */
   const [form, setForm] = useState({
@@ -63,23 +52,36 @@ function AppProvider({ children }) {
   const [filteredCards, setFilteredCards] = useState(cards);
   const [filter, setFilter] = useState("");
 
+  const [selected, setSelected] = useState({
+    category: '', type: '', order: {
+      type: '',
+      order: ''
+    }
+  });//lo agrego JeanHey para filtros de cards en el back
+
+  const tools = async () => {
+    const response = await axios.get("http://localhost:3000/api/admin/post");
+    return response.data;
+  };
+
   return (
     <AppContext.Provider
       value={{
+        selected,
+        setSelected,
         postDetail,
         setPostDetail,
         cards,
         setCards,
         title,
         setTitle,
-        selected,
-        setSelected,
         selectedType,
         setSelectedType,
         sortBy,
         setSortBy,
         searchTerm,
         setSearchTerm,
+        tools,
         filteredCards,
         setFilteredCards,
         rent,
@@ -113,3 +115,4 @@ function AppProvider({ children }) {
 }
 
 export { AppProvider, AppContext };
+
