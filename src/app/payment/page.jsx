a3bc98b7b32241e3413318b1e86d017666180a55
Gfrey <gfreycode@gmail.com>
2023-05-18 16:14:24 -0500

    Listo para la prueba de mercadopago

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,79 +1,135 @@
-'use client'
-import { useState } from 'react';
-import styles from './payment.module.css';
-import validateForm from './assets/validateForm';
+"use client";
+import { useState } from "react";
+import styles from "./payment.module.css";
+import validateForm from "./assets/validateForm";
+import payMercadoPago from "./assets/payMercadopago";
 
 function Payment() {
-    const [ disabled, setDisabled ] = useState(true);
-    const [ gateway, setGateway ] = useState(false);
+    const [disabled, setDisabled] = useState(true);
+    const [gateway, setGateway] = useState(false);
 
-    const [ form, setForm ] = useState({
-        fullname: '',
-        email: '',
-        phone: '',
-        address: '',
+    const [form, setForm] = useState({
+        fullname: "",
+        email: "",
+        phone: "",
+        address: "",
     });
 
-    const [ errors, setErros ] = useState({
-        fullname: '',
-        email: '',
-        phone: '',
-        address: '',
+    const [errors, setErros] = useState({
+        fullname: "",
+        email: "",
+        phone: "",
+        address: "",
     });
 
-    function handleForm(event){
+    function handleForm(event) {
         const name = event.target.name;
         const value = event.target.value;
-        setForm({...form, [name]: value});
+        setForm({ ...form, [name]: value });
         validateForm({ ...form, [name]: value }, errors, setErros, setDisabled);
-        
     }
 
-    return ( 
+    const body = {
+        items: cart,
+        payer: {
+            name: form.email
+        }
+    }
+
+    const temp = {
+        postDetail: 1,
+        items: [
+            {
+                title: "Martillo",
+                description: "Martillo usada 3 veces",
+                quantity: 5,
+                unit_price: 2,
+                currency_id: "USD",
+            },
+            {
+                title: "Destornillador",
+                description: "En buen estado, se utilizó una única vez",
+                quantity: 1,
+                unit_price: 5,
+                currency_id: "USD",
+            },
+        ],
+        payer: {
+            name: "test_user_1555855231@testuser.com",
+        },
+    };
+
+    return (
         <main>
             <section className={styles.form}>
                 <form action="">
                     <div>
                         <label htmlFor="">Nombre Completo:</label>
-                        <input onChange={handleForm} type="text" value={form.fullname} name='fullname'/>
+                        <input
+                            onChange={handleForm}
+                            type="text"
+                            value={form.fullname}
+                            name="fullname"
+                        />
                         <span>{errors.fullname}</span>
                     </div>
                     <div>
                         <label htmlFor="">Correo:</label>
-                        <input onChange={handleForm} type="email" value={form.email} name='email'/>
+                        <input
+                            onChange={handleForm}
+                            type="email"
+                            value={form.email}
+                            name="email"
+                        />
                         <span>{errors.email}</span>
                     </div>
                     <div>
                         <label htmlFor="">Celular:</label>
-                        <input onChange={handleForm} type="number" value={form.phone} name='phone'/>
+                        <input
+                            onChange={handleForm}
+                            type="number"
+                            value={form.phone}
+                            name="phone"
+                        />
                         <span>{errors.phone}</span>
                     </div>
                     <div>
                         <label htmlFor="">Direccion:</label>
-                        <input onChange={handleForm} type="text" value={form.address} name='address'/>
+                        <input
+                            onChange={handleForm}
+                            type="text"
+                            value={form.address}
+                            name="address"
+                        />
                         <span>{errors.address}</span>
                     </div>
                 </form>
             </section>
             <section className={styles.payment}>
                 <div className={styles.cart}>
-                    <div className={styles.cartItems}>
-
-                    </div>
+                    <div className={styles.cartItems}></div>
                     <div>
                         <h3>TOTAL:</h3>
                         <h3>TOTAL</h3>
                     </div>
                 </div>
                 <div className={styles.gateway}>
-                    <button onClick={()=>setGateway((state) => !state)} disabled={disabled}>Opciones de Pago</button>
-                    { gateway &&
+                    <button
+                        onClick={() => setGateway((state) => !state)}
+                        disabled={disabled}
+                    >
+                        Opciones de Pago
+                    </button>
+                    {gateway && (
                         <div>
                             <button>Contra Entrega</button>
                             <button>Tarjeta de credito</button>
-                            <button>Mercado Pago</button>
+                            <button
+                                onClick={() => payMercadoPago(temp)}>
+                                Mercado Pago
+                            </button>
                         </div>
-                    }
+                    )}
                 </div>
             </section>
         </main>
