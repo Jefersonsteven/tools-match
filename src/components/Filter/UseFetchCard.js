f4e85fc910001b2f981b74037c99c14e16e1fc44
heyller hinestroza <heyller-19@outlook.com>
2023-05-18 16:49:43 -0500

    filtrados y ordenamientos listos

---
+++ src/components/Filter/UseFetchCard.js
@@ -0,0 +1,36 @@
+export const fetchCards = async (selected, setCards) => {
+  const getCategoryParam = () => selected.category ? `category=${selected.category}` : '';
+  const getTypeParam = () => selected.type ? `type=${selected.type}` : '';
+
+  const categoryParam = getCategoryParam();
+  const typeParam = getTypeParam();
+  const orderParam = selected.order ? `order=${selected.order.order}&` : '';
+
+  const response = await fetch(`/api/filters/typeAndCategory?${categoryParam}&${typeParam}`);
+  const data = await response.json();
+  let cards = data || [];
+
+  if (selected.order?.type === 'price') {
+    const orderResponse = await fetch(`/api/orderings/orderPrice?${orderParam}${typeParam}&${categoryParam}`);
+    const orderData = await orderResponse.json();
+    cards = orderData || [];
+  }
+
+  if (selected.order?.type === 'alpha') {
+    const orderResponse = await fetch(`/api/orderings/orderAlphabetically?${orderParam}${typeParam}&${categoryParam}`);
+    const orderData = await orderResponse.json();
+    cards = orderData || [];
+  }
+  if (selected.order?.type === 'rating') {
+    const orderResponse = await fetch(`/api/orderings/orderRating?${orderParam}${typeParam}&${categoryParam}`);
+    const orderData = await orderResponse.json();
+    cards = orderData || [];
+  }
+  if(selected.category=="" && selected.type=="" && selected.order.type=="" && selected.order.order=="") {
+    const orderResponse = await fetch(`/api/admin/post`);
+    const orderData = await orderResponse.json();
+    cards = orderData || [];
+  }
+
+  setCards(cards);
+};
