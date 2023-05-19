a8c56c25775c97e79b8d6efe03ce64f56c61dd10
Franco Itria <francoitria01@gmail.com>
2023-05-19 17:06:44 -0300

    Se crea el pago del usuario

--- pages/api/mercadopago/pago/index.js
+++ pages/api/mercadopago/pago/index.js
@@ -6,13 +6,22 @@ export default async function handler(req, res) {
     try {
       const response = await mercadopago.payment.get(req.body.data.id);
       const name = response.body.additional_info.payer.first_name;
-      const URL_BASE = process.env.DEPLOY_BACK || 'http://localhost:3000'
+      const URL_BASE = process.env.DEPLOY_BACK || "http://localhost:3000";
       const responseUser = await fetch(`${URL_BASE}/api/user/${name}`);
       const user = await responseUser.json();
       if (user) {
-        console.log('Costo total: ' + response.body.transaction_amount)
-        console.log('Tipo de moneda: ' + response.body.currency_id)
-        console.log(response.body)
+        await fetch(`${URL_BASE}/api/payment`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            amount: response.body.transaction_amount,
+            currency: response.body.currency_id,
+            userId: user.id,
+          }),
+        });
+        res.status(200).json({ success: true });
       } else
         throw new Error("No se va a poder registrar su compra en el historial");
     } catch (error) {
