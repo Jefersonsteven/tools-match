d9d409456bcc82f9217b03c470fb054c93ba246c
Axel Valiente <axelvalientexd@gmail.com>
2023-05-18 21:11:54 -0300

    working in edit user and bugs in edit fixed

--- src/app/perfil/[perfilId]/edit/page.jsx
+++ src/app/perfil/[perfilId]/edit/page.jsx
@@ -22,13 +22,13 @@ const EditUser = () => {
     saveInLocalStorage,
   } = useContext(AppContext);
   const [form, setForm] = useState({
-    firstname: userData.firstname,
-    lastname: userData.lastname,
-    email: userData.email,
-    phoneNumber: userData.phoneNumber,
-    country: userData.country,
-    zipCode: userData.zipCode,
-    photo: userData.photo,
+    firstname: userData?.firstname,
+    lastname: userData?.lastname,
+    email: userData?.email,
+    phoneNumber: userData?.phoneNumber,
+    country: userData?.country,
+    zipCode: userData?.zipCode,
+    photo: userData?.photo,
   });
   const [selectedFile, setSelectedFile] = useState(null);
   const [isActive, setIsActive] = useState(false);
@@ -67,8 +67,8 @@ const EditUser = () => {
         console.log(response);
         if (!response.error) {
           setInPetition("Cambios aplicados correctamente");
-          setUserData(response);
-          saveInLocalStorage("token", response);
+          setUserData({ ...userData, ...response });
+          saveInLocalStorage("token", { ...userData, ...response });
           push(`perfil/${userData.id}`);
           const Toast = Swal.mixin({
             toast: true,
@@ -119,122 +119,139 @@ const EditUser = () => {
 
   return (
     <>
-      <div>
-        <Link href={`/perfil/${userId}`}>
-          <IoCaretBack size={50} className={styles.IoCaretBack} />
-        </Link>
-        <h2>Editar perfil de toolmatch</h2>
-      </div>
-      <section className={styles.section}>
-        <div className={styles.imageContainer}>
-          <div className={styles.containers}>
-            <ImageUploader setSelectedFile={setSelectedFile} form={form} />
-            <Link
-              href={`perfil/${userId}/changePassword`}
-              className={styles.changePwdButton}
-            >
-              Cambiar contraseña
+      {userData && (
+        <>
+          <div>
+            <Link href={`/perfil/${userId}`}>
+              <IoCaretBack size={50} className={styles.IoCaretBack} />
             </Link>
+            <h2 className={styles.editTitle}>Editar perfil de toolmatch</h2>
           </div>
-        </div>
-        <div className={styles.formContainer}>
-          <form className={styles.editUserDataForm} onSubmit={handleSubmit}>
-            <div className={styles.inputsContainer}>
-              <div className={styles.inputContainer}>
-                <label htmlFor="firstname">Nombre:</label>
-                <input
-                  type="text"
-                  name="firstname"
-                  value={form.firstname}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className={styles.inputContainer}>
-                <label htmlFor="lastname">Apellido:</label>
-                <input
-                  type="text"
-                  name="lastname"
-                  value={form.lastname}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-            <div className={styles.inputsContainer}>
-              <div className={styles.inputContainer}>
-                <label htmlFor="email">Correo:</label>
-                <input type="text" name="email" disabled value={form.email} />
-              </div>
-              <div className={styles.inputContainer}>
-                <label htmlFor="phoneNumber">Celular:</label>
-                <input
-                  type="text"
-                  name="phoneNumber"
-                  value={form.phoneNumber && form.phoneNumber}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-            <div className={styles.inputsContainer}>
-              <div className={styles.selectContainer}>
-                <label htmlFor="country">País donde resides actualmente:</label>
-
-                <select
-                  id="country"
-                  name="country"
-                  defaultValue={form.country ? form.country : "null"}
-                  onChange={handleChange}
+          <section className={styles.section}>
+            <div className={styles.imageContainer}>
+              <div className={styles.containers}>
+                <ImageUploader setSelectedFile={setSelectedFile} form={form} />
+                <Link
+                  href={`perfil/${userId}/changePassword`}
+                  className={styles.changePwdButton}
                 >
-                  {Object.entries(countries).map(([property, value]) => {
-                    return (
-                      <option key={property} value={property}>
-                        {value}
-                      </option>
-                    );
-                  })}
-                </select>
+                  Cambiar contraseña
+                </Link>
               </div>
-              <div className={styles.inputContainer}>
-                <label htmlFor="zipCode">Código postal</label>
+            </div>
+            <div className={styles.formContainer}>
+              <form className={styles.editUserDataForm} onSubmit={handleSubmit}>
+                <div className={styles.inputsContainer}>
+                  <div className={styles.inputContainer}>
+                    <label htmlFor="firstname">Nombre:</label>
+                    <input
+                      type="text"
+                      name="firstname"
+                      value={form.firstname}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className={styles.inputContainer}>
+                    <label htmlFor="lastname">Apellido:</label>
+                    <input
+                      type="text"
+                      name="lastname"
+                      value={form.lastname}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className={styles.inputsContainer}>
+                  <div className={styles.inputContainer}>
+                    <label htmlFor="email">Correo:</label>
+                    <input
+                      type="text"
+                      name="email"
+                      disabled
+                      value={form.email}
+                    />
+                  </div>
+                  <div className={styles.inputContainer}>
+                    <label htmlFor="phoneNumber">Celular:</label>
+                    <input
+                      type="text"
+                      name="phoneNumber"
+                      value={form.phoneNumber && form.phoneNumber}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className={styles.inputsContainer}>
+                  <div className={styles.selectContainer}>
+                    <label htmlFor="country">
+                      País donde resides actualmente:
+                    </label>
 
-                <input
-                  type="text"
-                  name="zipCode"
-                  value={form.zipCode && form.zipCode}
-                  onChange={handleChange}
-                />
-              </div>
+                    <select
+                      id="country"
+                      name="country"
+                      defaultValue={form.country ? form.country : "null"}
+                      onChange={handleChange}
+                    >
+                      {Object.entries(countries).map(([property, value]) => {
+                        return (
+                          <option key={property} value={property}>
+                            {value}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
+                  <div className={styles.inputContainer}>
+                    <label htmlFor="zipCode">Código postal</label>
+
+                    <input
+                      type="text"
+                      name="zipCode"
+                      value={form.zipCode && form.zipCode}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className={styles.inputContainer}>
+                  <label htmlFor="password">
+                    Contraseña para aplicar cambios:
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className={styles.buttonsContainer}>
+                  <button
+                    className={styles.buttonCancel}
+                    onClick={handleCancel}
+                    disabled={isActive}
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    className={styles.buttonSubmit}
+                    type="submit"
+                    disabled={isActive}
+                  >
+                    Aplicar cambios
+                  </button>
+                </div>
+                <div className={styles.loaderContainer}>
+                  {isActive && (
+                    <>
+                      <Loader />
+                      <p>{inPetition && inPetition}</p>
+                    </>
+                  )}
+                </div>
+              </form>
             </div>
-            <div className={styles.inputContainer}>
-              <label htmlFor="password">Contraseña para aplicar cambios:</label>
-              <input type="password" name="password" onChange={handleChange} />
-            </div>
-            <div className={styles.buttonsContainer}>
-              <button
-                className={styles.buttonCancel}
-                onClick={handleCancel}
-                disabled={isActive}
-              >
-                Cancelar
-              </button>
-              <button
-                className={styles.buttonSubmit}
-                type="submit"
-                disabled={isActive}
-              >
-                Aplicar cambios
-              </button>
-            </div>
-            <div className={styles.loaderContainer}>
-              {isActive && (
-                <>
-                  <Loader />
-                  <p>{inPetition && inPetition}</p>
-                </>
-              )}
-            </div>
-          </form>
-        </div>
-      </section>
+          </section>
+        </>
+      )}
     </>
   );
 };
--- src/app/perfil/[perfilId]/page.jsx
+++ src/app/perfil/[perfilId]/page.jsx
@@ -8,85 +8,106 @@ import { AppContext } from "@/context/AppContext";
 
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function PerfilUsuario() {
   const [editingUser, setEditingUser] = useState(null);
+  const { push } = useRouter();
 
   const { userId, userData, countries } = useContext(AppContext);
 
+  useEffect(() => {
+    !userData && push("/");
+  }, [userData]);
+
   return (
     <>
-      <h2>Perfil de toolmatch</h2>
-      <section className={styles.section}>
-        <div className={styles.userContainer}>
-          <div className={styles.userDataContainer}>
-            <div className={styles.imageContainer}>
-              <Image
-                src={
-                  userData.photo
-                    ? userData.photo
-                    : "/assets/userPhotoDefault.png"
-                }
-                width={254}
-                height={254}
-                alt="Uploaded"
-              />
+      {userData && (
+        <>
+          <h2 className={styles.sectionTitle}>Perfil de toolmatch</h2>
+          <section className={styles.section}>
+            <div className={styles.userContainer}>
+              <div className={styles.userDataContainer}>
+                <div className={styles.imageContainer}>
+                  <Image
+                    src={
+                      userData.photo
+                        ? userData.photo
+                        : "/assets/userPhotoDefault.png"
+                    }
+                    width={254}
+                    height={254}
+                    alt="Uploaded"
+                  />
+                </div>
+                <div>
+                  <h2>
+                    <strong>Usuario: </strong>
+                    {`@${userData.firstname}`}
+                  </h2>
+                  <h2>
+                    <strong>Nombre completo: </strong>
+                    {`${userData.firstname} ${userData.lastname}`}
+                  </h2>
+                  <h2>
+                    <strong>Correo: </strong>
+                    {userData.email}
+                  </h2>
+                  <h2>
+                    <strong>País de residencia: </strong>
+                    {userData.country ? countries[userData.country] : "---"}
+                  </h2>
+                  <h2>
+                    <strong>Código postal: </strong>
+                    {userData.zipCode ? userData.zipCode : "---"}
+                  </h2>
+                  <h2>
+                    <strong>Celular: </strong>
+                    {userData.phoneNumber ? userData.phoneNumber : "---"}
+                  </h2>
+                </div>
+              </div>
+              <div className={styles.buttonContainer}>
+                <Link
+                  href={`perfil/${userId}/edit`}
+                  className={styles.editButton}
+                >
+                  Editar datos
+                </Link>
+              </div>
             </div>
-            <div>
-              <h2>
-                <strong>Usuario: </strong>
-                {`@${userData.firstname}`}
-              </h2>
-              <h2>
-                <strong>Nombre completo: </strong>
-                {`${userData.firstname} ${userData.lastname}`}
-              </h2>
-              <h2>
-                <strong>Correo: </strong>
-                {userData.email}
-              </h2>
-              <h2>
-                <strong>País de residencia: </strong>
-                {userData.country ? countries[userData.country] : "---"}
-              </h2>
-              <h2>
-                <strong>Código postal: </strong>
-                {userData.zipCode ? userData.zipCode : "---"}
-              </h2>
-              <h2>
-                <strong>Celular: </strong>
-                {userData.phoneNumber ? userData.phoneNumber : "---"}
-              </h2>
-            </div>
+          </section>
+          <div className={styles.sectionsContainer}>
+            <section>
+              <h3 className={styles.sectionTitleH3}>Herramientas Publicadas</h3>
+              <div className="grid grid-cols-4 gap-9">
+                {userData.posts.length ? (
+                  userData.posts.map((post) => {
+                    return (
+                      <Card
+                        key={post.id}
+                        photo={post.photo[0]}
+                        title={post.title}
+                        price={post.price}
+                        type={`${
+                          post.type === "RENTAL" ? "Arriendo" : "Venta"
+                        }`}
+                        perDay={post.perDay}
+                        id={post.id}
+                      />
+                    );
+                  })
+                ) : (
+                  <p>No hay productos publicados</p>
+                )}
+              </div>
+            </section>
+            <section>
+              <h3 className={styles.sectionTitleH3}>Compras y Arriendos</h3>
+            </section>
           </div>
-          <div className={styles.buttonContainer}>
-            <Link href={`perfil/${userId}/edit`} className={styles.editButton}>
-              Editar datos
-            </Link>
-          </div>
-        </div>
-      </section>
-      <section>
-        <h2>Tienda</h2>
-        <div>
-          {userData.posts.length ? (
-            userData.posts.map((post) => {
-              return (
-                <Card
-                  key={post.id}
-                  photo={post.photo[0]}
-                  title={post.title}
-                  price={post.price}
-                  type={`${post.type === "RENTAL" ? "Arriendo" : "Venta"}`}
-                  id={post.id}
-                />
-              );
-            })
-          ) : (
-            <p>No hay productos publicados</p>
-          )}
-        </div>
-      </section>
+        </>
+      )}
     </>
   );
 }
