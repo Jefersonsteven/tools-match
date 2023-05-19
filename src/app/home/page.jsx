6a9aec5ec1d64625f2a853f5e22e699c22ca2c3a
Adri-ESM <anggivmorales@gmail.com>
2023-05-18 21:03:51 -0400

    paginated in the all those components and views

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -2,14 +2,32 @@
 import Cards from "@/components/Cards/Cards";
 import FilterBar from "@/components/Filter/FilterBar";
 import Paginated from "@/components/paginated/Paginated";
+import style from "./Home.module.css";
+import Image from "next/image";
 
-function Home({cards, currentPage, setCards, setCurrentPage } ) {
-
+function Home({ cards, currentPage, setCards, setCurrentPage }) {
   return (
     <div className="flex flex-col mt-8 p-28 min-h-screen">
-      <h1 className="text-4xl font-bold text-center m-5">
+      <div className={style.container}>
+        <div>
+          <h1 className={style.publicity}>
+            Unete a la Comunidad TOOLSMATCH <br /> !Registrate hoy mismo y
+            comienza a aprovechar todas las ventajas de nuestra Comunidad de
+            compradores y vendedores locales!
+          </h1>
+        </div>
+        <div className={style.image}>
+          <Image
+            src="/images/image/construction.png"
+            alt="ToolsMatch"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
+      <h2 className="text-4xl font-bold text-center m-5">
         Renta o Compra Herramientas en Tu Comunidad
-      </h1>
+      </h2>
       <div className="container mx-auto px-4">
         <div className="bg-green-80 flex justify-between py-4 px-4 sm:px-6 lg:px-8">
           <FilterBar />
@@ -19,7 +37,7 @@ function Home({cards, currentPage, setCards, setCurrentPage } ) {
             <Cards className="mb-1" />
           </div>
         </div>
-      </div>     
+      </div>
     </div>
   );
 }
