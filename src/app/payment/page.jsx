ee4f4c7f20115076a89d5c846bdb89acf55c93cd
Gfrey <gfreycode@gmail.com>
2023-05-20 18:48:41 -0500

    post, createpost, cart, payment connecting

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -11,33 +11,66 @@ import { useEffect } from "react";
 function Page() {
   const [disabled, setDisabled] = useState(true);
   const [gateway, setGateway] = useState(false);
-  const { userData } = useContext(AppContext);
+  const [form, setForm] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+  });
+  const [errors, setErros] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+  });
+  const { userData, cart, setCart } = useContext(AppContext);
   const params = useSearchParams();
   const router = useRouter();
 
   useEffect(() => {
-    // if (!userData?.name) router.push("/form/login");
+    if (!userData.id) {
+      router.push("/form/login");
+    }
+
     const status = params.get("status");
     if (status === "approved") {
-      // se borra el estado del carrito
-      // se redirecciona al del perfil
-      console.log("Vamos al perfil");
+      setCart({
+        count: 0,
+        items: [],
+      });
+
+      if (typeof window !== "undefined") {
+        localStorage.setItem(
+          "cart",
+          JSON.stringify({
+            count: 0,
+            items: [],
+          })
+        );
+      }
+
+      Swal.fire({
+        title: "¡Compra aprobada!",
+        text: "Tu compra ha sido aprobada correctamente.",
+        icon: "success",
+      }).then(() => {
+        router.push(`/perfil/${userData.id}`);
+      });
     }
-  }, [params, router, userData]);
+  }, [params, router, setCart, userData]);
 
-  const [form, setForm] = useState({
-    fullname: "",
-    email: "",
-    phone: "",
-    address: "",
-  });
-
-  const [errors, setErros] = useState({
-    fullname: "",
-    email: "",
-    phone: "",
-    address: "",
-  });
+  useEffect(() => {
+    if (!form.fullname) {
+      const FORM = {
+        fullname: `${userData.firstname} ${userData.lastname}`,
+        email: userData.email,
+        phoneNumber: userData.phoneNumber,
+        address: "",
+      };
+      setForm(FORM);
+      validateForm(FORM, errors, setErros);
+    }
+  }, [userData, form, errors]);
 
   function handleForm(event) {
     const name = event.target.name;
@@ -46,39 +79,47 @@ function Page() {
     validateForm({ ...form, [name]: value }, errors, setErros, setDisabled);
   }
 
+  const body = {
+    items: cart.items.map(({ title, content, price }) => {
+      return {
+        title,
+        content,
+        quantity: 1,
+        unit_price: price,
+        currency_id: "USD",
+      };
+    }),
+    payer: {
+      name: userData.email,
+    },
+  };
+
   async function handleMercadoPago() {
-    const { LinkDePagoSandbox, LinkDePagoInit } = await payMercadoPago(temp);
+    const { LinkDePagoSandbox, LinkDePagoInit } = await payMercadoPago(body);
     window.location.href = LinkDePagoSandbox;
   }
 
-  // const body = {
-  //     items: cart,
-  //     payer: {
-  //         name: form.email
-  //     }
-  // }
-
-  const temp = {
-    items: [
-      {
-        title: "Martillo",
-        content: "Martillo usada 3 veces",
-        quantity: 5,
-        unit_price: 2,
-        currency_id: "USD",
-      },
-      {
-        title: "Destornillador",
-        description: "En buen estado, se utilizó una única vez",
-        quantity: 1,
-        unit_price: 5,
-        currency_id: "USD",
-      },
-    ],
-    payer: {
-      name: "test_user_1555855231@testuser.com",
-    },
-  };
+  // const temp = {
+  //   items: [
+  //     {
+  //       title: "Martillo",
+  //       content: "Martillo usada 3 veces",
+  //       quantity: 5,
+  //       unit_price: 2,
+  //       currency_id: "USD",
+  //     },
+  //     {
+  //       title: "Destornillador",
+  //       description: "En buen estado, se utilizó una única vez",
+  //       quantity: 1,
+  //       unit_price: 5,
+  //       currency_id: "USD",
+  //     },
+  //   ],
+  //   payer: {
+  //     name: "test_user_1555855231@testuser.com",
+  //   },
+  // };
 
   return (
     <main className={styles.container}>
@@ -109,10 +150,10 @@ function Page() {
             <input
               onChange={handleForm}
               type="number"
-              value={form.phone}
+              value={form.phoneNumber}
               name="phone"
             />
-            <span>{errors.phone}</span>
+            <span>{errors.phoneNumber}</span>
           </div>
           <div>
             <label htmlFor="">Direccion:</label>
@@ -128,10 +169,17 @@ function Page() {
       </section>
       <section className={styles.payment}>
         <div className={styles.cart}>
-          <div className={styles.cartItems}></div>
+          <div className={styles.cartItems}>
+            {cart.items.map((item) => (
+              <div key={item.id} className={styles.item}>
+                <h4>{item.title}</h4>
+                <h4>{item.price}</h4>
+              </div>
+            ))}
+          </div>
           <div className={styles.total}>
             <h3>TOTAL:</h3>
-            <h3>TOTAL</h3>
+            <h3>{cart.items.reduce((acc, item) => acc + item.price, 0)}</h3>
           </div>
         </div>
         <div className={styles.gateway}>
