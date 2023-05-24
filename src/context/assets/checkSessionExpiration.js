86aba9161b3447f714ead4954d8059ff4e873bb2
Emanuel Burgos <emaburgos17@hotmail.com>
2023-05-24 10:17:12 -0300

    dashboardprueba

--- src/context/assets/checkSessionExpiration.js
+++ src/context/assets/checkSessionExpiration.js
@@ -5,7 +5,7 @@ const checkSessionExpiration = () => {
   const loginTime = localStorage.getItem("loginTime");
   if (loginTime) {
     const currentTime = new Date().getTime();
-    const expirationTime = 30 * 1000;
+    const expirationTime = 24 * 60 * 60 * 1000;
     if (currentTime - loginTime > expirationTime) {
       localStorage.removeItem("loginTime");
       localStorage.removeItem("token");
