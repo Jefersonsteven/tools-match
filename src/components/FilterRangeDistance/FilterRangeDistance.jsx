9d7842206cdea6bd36c415e31871999029e9de28
Jefersonsteven <gfreycode@gmail.com>
2023-05-26 05:32:31 -0500

    arregle favoritos en el header, agregue la peticion e ubicacion para filtrar por distancia

--- src/components/FilterRangeDistance/FilterRangeDistance.jsx
+++ src/components/FilterRangeDistance/FilterRangeDistance.jsx
@@ -2,13 +2,32 @@ import { useContext, useState } from "react";
 import styles from "./FilterRangeDistance.module.css";
 import { AppContext } from "@/context/AppContext";
 import axios from "axios";
+import { getLocation } from "@/app/crear-publicacion/asset/getLocation";
 
 function FilterRangeDistance({ handleKm }) {
   const { userData } = useContext(AppContext);
   const [range, setRange] = useState(4000);
 
+  async function coords(lat, long) {
+    try {
+      const mapImage = await axios.post("/api/maps", { lat, long });
+      const addMap = await axios.put(`/api/user/${userData.email}`, {
+        map: mapImage.data,
+      });
+      const coordinates = await axios.put(`/api/user/${userData.email}`, {
+        coordinates: [lat.toString(), long.toString()],
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   async function exitInput() {
     if (userData) {
+      const position = await getLocation();
+      const { latitude, longitude } = position.coords;
+      await coords(latitude, longitude);
+
       const user = await axios.get(`/api/user/${userData.email}`);
       const { coordinates } = user.data;
       if (coordinates[0] && coordinates[1]) {
