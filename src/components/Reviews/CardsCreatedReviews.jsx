632816353131054534b6511950981f962ecf932c
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-05-28 07:54:30 -0300

    botones update delete ok

--- src/components/Reviews/CardsCreatedReviews.jsx
+++ src/components/Reviews/CardsCreatedReviews.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { RiStarFill, RiStarLine } from "react-icons/ri";
+import { TiDelete, TiPencil } from "react-icons/ti";
 import Image from "next/image";
+import styles from "./CardsCreatedReviews.module.css"; // Importamos el archivo CSS local
 
 const CardsCreatedReviews = ({ createdReviews, author }) => {
-
   return (
     <div className="grid grid-cols-3 gap-4 bg-green-80">
-      {createdReviews.map((review) => {       
+      {createdReviews.map((review) => {
         const starCount = 5;
         const rating = review.rating;
 
         return (
           <div
-            className="card rounded-md overflow-hidden shadow-md bg-white"
+            className="card rounded-md overflow-hidden shadow-md bg-white relative" // Agregamos la clase "relative" para posicionar los botones correctamente
             key={review.id}
           >
             <div className="user-info flex items-center p-4">
@@ -53,6 +54,22 @@ const CardsCreatedReviews = ({ createdReviews, author }) => {
               </div>
             </div>
             <p className="review-content px-4 pb-4">{review.content}</p>
+
+            {/* Botones de edición */}
+            <div className="absolute top-2 right-2 flex">
+              <button
+                className={`${styles.iconButton} p-1 bg-transparent border-none`} // Aplicamos la clase personalizada y los estilos al botón
+                onClick={() => handleDelete(review.id)}
+              >
+                <TiDelete className="text-red-500 text-xl" />
+              </button>
+              <button
+                className={`${styles.iconButton} p-1 bg-transparent border-none`} // Aplicamos la clase personalizada y los estilos al botón
+                onClick={() => handleEdit(review.id)}
+              >
+                <TiPencil className="text-blue-500 text-xl" />
+              </button>
+            </div>
           </div>
         );
       })}
