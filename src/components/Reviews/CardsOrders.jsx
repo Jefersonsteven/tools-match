d5df269215367d28adb2aeca84330f9a6b34a350
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-05-30 06:46:07 -0300

    resenia ya dejada, alert eliminar

--- src/components/Reviews/CardsOrders.jsx
+++ src/components/Reviews/CardsOrders.jsx
@@ -7,7 +7,6 @@ import styles from "./CardsOrders.module.css";
 import CardsPurchasedItems from "./CardsPurchasedItems";
 
 const CardsOrders = ({ userOrders }) => {
-
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     const options = { day: "numeric", month: "long" };
@@ -15,17 +14,7 @@ const CardsOrders = ({ userOrders }) => {
   };
 
   const [selectedOrder, setSelectedOrder] = useState(null);
-
-  const type = async (postArray) => {
-    const newArray = [];  
-    for (const post of postArray) {
-        const postResponse = await axios.get(`/api/admin/post/${post}`);
-        const typolin = postResponse.type;
-        newArray.push(typolin);
-      }     
-    return newArray;
-  };
-    
+  const [reviewSubmitted, setReviewSubmitted] = useState(false); // Nuevo estado
 
   const closeModal = () => {
     setSelectedOrder(null);
@@ -60,7 +49,6 @@ const CardsOrders = ({ userOrders }) => {
               </h4>
             </div>
             <div className="flex flex-col items-end">
-              {/* <h3> {type(order.postId)}</h3> */}
               <h3 className="text-black font-bold text-3xl">$ {order.amount}</h3>
               <h4 className="text-gray-400 text-lg mt-2">{formatDate(order.date)}</h4>
             </div>
@@ -77,7 +65,12 @@ const CardsOrders = ({ userOrders }) => {
         {selectedOrder && (
           <>
             <div className={styles.modalContent}>
-              <CardsPurchasedItems orderPosts={selectedOrder.postId} types={selectedOrder.types} onClose={closeModal}/>
+              <CardsPurchasedItems
+                orderPosts={selectedOrder.postId}
+                orderDate={selectedOrder.date}
+                onClose={closeModal}
+                reviewSubmitted={reviewSubmitted} // Pasar la propiedad reviewSubmitted
+              />
             </div>
           </>
         )}
